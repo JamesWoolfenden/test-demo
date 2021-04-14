2ee3dd84004d85a0380760d3236fe4c8bda409ed
James Woolfenden <james.woolfenden@gmail.com>
2021-04-14 10:30:25 +0100

    test

--- azure_storage_account.demo.tf
+++ azure_storage_account.demo.tf
@@ -1,4 +1,5 @@
 resource "azurerm_storage_account" "demo" {
+  # checkov:skip=CKV_AZURE_35: ADD REASON
   # checkov:skip=CKV_AZURE_43: dont care a jot about this
 
   enable_https_traffic_only = true
